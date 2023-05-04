a8dbcd43d76fd3c3c971a7fc4148a50b307b6757
Islom <123618830+jdu214200@users.noreply.github.com>
2023-05-04 13:50:36 +0300

    Add files via upload

---
+++ 21D/check.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+    $image = $_FILES['image'];
+
+
+    move_uploaded_file($image['tmp_name'], 'images/' .$name. "JPG");
+    echo "<p>Name"
+</body>
+</html>
---
+++ 21D/read.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <form action="create.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="first name" placeholder= "firstname"> <br><br>
+        <input type="file" name="image"> <br><br>
+        <input type="submit" value ="save" > <br><br>
+    </form>
+</body>
+</html>
