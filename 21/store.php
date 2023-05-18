71a28034c153a81d5823bf451ea11165279e60eb
Islom <123618830+jdu214200@users.noreply.github.com>
2023-05-18 10:04:28 +0300

    Update store.php

---
+++ 21/store.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD</title>
+</head>
+<body>
+    <h1>CRUD operation</h1>
+    <form action="check.php" method="POST" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="name">
+        <br><br>
+        <input type="password" name="password" placeholder="password"> 
+        <br><br>
+        <input type="file" name="image">
+        <br><br>
+        <input type="submit" value="Login">
+    </form>
+    </body>
+</html>
