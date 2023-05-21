503f0169ab52507c1faf05b453531bd96d701847
Islom <123618830+jdu214200@users.noreply.github.com>
2023-05-21 20:16:57 +0300

    Add files via upload

---
+++ royhati/create.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create</title>
+</head>
+<body>
+    <h1>Create a student</h1>
+    <form action="store.php">
+        <input type="text" name="f" placeholder="Firstname">
+        <br><br>
+        <input type="text" name="l" placeholder="Lastname">
+        <br><br>
+        <input type="submit" value="Add" >
+        <br><br>
+    </form>
+</body>
