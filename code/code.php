cc57e9d0ec0763ab62a481d10157ec80efe99634
Islom <123618830+jdu214200@users.noreply.github.com>
2023-07-06 20:55:31 +0300

    Add files via upload

---
+++ code/code.php
@@ -0,0 +1,21 @@
+Sure, here's an example of a simple PHP code snippet:
+
+```php
+<?php
+// Define a variable
+$name = "John Doe";
+
+// Print a message using the variable
+echo "Hello, " . $name . "!";
+
+// Perform a calculation
+$number1 = 10;
+$number2 = 5;
+$sum = $number1 + $number2;
+
+// Print the result
+echo "The sum of " . $number1 . " and " . $number2 . " is " . $sum . ".";
+?>
+```
+
+This code snippet demonstrates basic PHP syntax. It defines a variable `$name` and prints a message using that variable. It also performs a calculation by adding two numbers (`$number1` and `$number2`) and then prints the result.
